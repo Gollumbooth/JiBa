4c81eaa52166af2a65865ebbf87e59f21dc6a90a
Tommy <tommy273@gmail.com>
2024-11-23 21:05:15 +1300

    Two JiBa add to the game

--- src/main/java/com/gangy/jiba/item/ModFoodComponents.java
+++ src/main/java/com/gangy/jiba/item/ModFoodComponents.java
@@ -6,14 +6,16 @@ import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
     public static final FoodComponent DJJ = new FoodComponent.Builder().hunger(5).saturationModifier(3.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 10), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3000, 10), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 3000, 6), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3000, 5), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 3000, 4), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 3000, 4), 1.0f)
             .build();
 
     public static final FoodComponent XJJ = new FoodComponent.Builder().hunger(3).saturationModifier(2.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 5), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3000, 5), 1.0f)
-            .statusEffect(new StatusEffectInstance(StatusEffects.HASTE, 3000, 4), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.STRENGTH, 3000, 3), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.RESISTANCE, 3000, 3), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST, 3000, 2), 1.0f)
+            .statusEffect(new StatusEffectInstance(StatusEffects.REGENERATION, 3000, 2), 1.0f)
             .build();
 }
